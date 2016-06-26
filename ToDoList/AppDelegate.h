381dbbf377a33b92c5c63b3ad1b80d0048a77fc9
Martin Denci <denci.martin@gmail.com>
2016-06-26 21:25:25 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Martin Denci on 26/06/16.
+//  Copyright © 2016 Martin Denci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Martin Denci on 26/06/16.
+//  Copyright © 2016 Martin Denci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
